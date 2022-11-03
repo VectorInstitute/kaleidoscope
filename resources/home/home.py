44079f9c84ef87c7a1ee96c15e66cb88fc61ac7b
jsiva <jsiva@v.cluster.local>
2022-11-03 18:34:50 -0400

    Add clear and active model features

--- resources/home/home.py
+++ resources/home/home.py
@@ -4,6 +4,8 @@ from flask_jwt_extended import jwt_required
 from models import ALL_MODELS
 
 ALL_MODEL_NAMES = set(ALL_MODELS.keys())
+MODEL_INSTANCES= {'OPT', 'GPT2'} # Hard coded available models, will be udpated dynamically
+
 home_bp = Blueprint("home", __name__, template_folder='templates')
 
 def verify_request(model_name):
@@ -26,4 +28,4 @@ async def home():
 @home_bp.route("/playground", methods=["GET"])
 async def playground():
     #  return f"sample inference server for models: {set(ALL_MODELS.keys())}"
-    return render_template("playground.html", models=ALL_MODEL_NAMES)
+    return render_template("playground.html", all_models=ALL_MODEL_NAMES, active_models=MODEL_INSTANCES)
