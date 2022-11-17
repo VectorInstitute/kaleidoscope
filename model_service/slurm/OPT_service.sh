51a433f5d265a46b5a62cdbb8c029ece554013c4
Mark Coatsworth <mark.coatsworth@vectorinstitute.ai>
2022-11-17 12:45:39 -0500

    Fixed OPT slurm submission script

--- model_service/slurm/OPT_service.sh
+++ model_service/slurm/OPT_service.sh
@@ -7,7 +7,7 @@
 #SBATCH --output=OPT_service.%j.out
 #SBATCH --error=OPT_service.%j.err
 
-cd /h/coatsworth/scratch/slurm/model_service/LLM
 module load singularity-ce/3.8.2
 export MASTER_ADDR=localhost
-singularity exec --nv --bind /checkpoint,/scratch /ssd003/projects/aieng/opt-125-latest.sif /usr/bin/python3 -s /h/coatsworth/scratch/slurm/model_service/LLM/model_service/model_service.py --model_type OPT
+cd /ssd003/projects/aieng
+singularity exec --nv --bind /checkpoint,/scratch,/ssd003 ./opt-125-latest.sif /usr/bin/python3 -s /ssd003/projects/aieng/lingua/model_service/model_service.py --model_type OPT
