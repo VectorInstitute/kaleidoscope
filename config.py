6d6c21205ec4f9fd2b9132a62f7b835606101642
Mark Coatsworth <mark.coatsworth@vectorinstitute.ai>
2022-11-30 11:36:19 -0500

    Hooked up login form

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config(object):
 
     JWT_SECRET_KEY = 'abc123'
 
-    LDAP_HOST = "172.17.15.252"
+    LDAP_HOST = "172.17.15.251"
     LDAP_BIND_DIRECT_PREFIX = 'uid='
     LDAP_BIND_DIRECT_SUFFIX = ',ou=People,dc=vector,dc=local'
     LDAP_BIND_DIRECT_GET_USER_INFO = False
--- resources/home/home.py
+++ resources/home/home.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, render_template, request
 from flask_jwt_extended import jwt_required
+import requests
 
 from models import ALL_MODELS
 from resources.models import MODEL_INSTANCES
@@ -25,6 +26,20 @@ async def heartbeat():
 async def home():
     return render_template("home.html")
 
+@home_bp.route("/login", methods=["GET", "POST"])
+async def login():
+    # GET requests display the login form
+    if request.method == "GET":
+        return render_template("login.html")
+
+    # POST requests handle the login
+    auth_url = request.url_root + "authenticate"
+    result = requests.post(auth_url, auth=(request.form["username"], request.form["password"]))
+    if result.status_code == 401:
+        return render_template("login.html", result=result)
+
+    return redirect(request.url_root, 200)
+
 @home_bp.route("/reference", methods=["GET"])
 async def reference():
     return render_template("reference.html")
@@ -33,4 +48,3 @@ async def reference():
 async def playground():
     #  return f"sample inference server for models: {set(ALL_MODELS.keys())}"
     return render_template("playground.html", all_models=ALL_MODEL_NAMES, active_models=MODEL_INSTANCES)
-
