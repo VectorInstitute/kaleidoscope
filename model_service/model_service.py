9df50930a8289517c01cfbdeb74d4d251536ed55
Omkar Dige <omkar.dige@vectorinstitute.ai>
2023-03-20 15:13:02 -0400

    Raise HTTP error if batch request size exceeds prescribed limit

--- model_service/model_service.py
+++ model_service/model_service.py
@@ -5,6 +5,7 @@ import signal
 import socket
 import sys
 import torch
+import urllib
 import os
 
 from flask import Flask, request, jsonify
@@ -35,8 +36,14 @@ def module_names():
 
 @service.route("/generate", methods=["POST"])
 def generate_text():
-    result = model.generate(request)
-    return result
+    batch_limit = 8
+    batch_request_size = len(request.json["prompt"])
+    if batch_request_size > batch_limit:
+        # Limit number of prompts in batch request
+        raise urllib.error.HTTPError(400, f"Current request batch size of {batch_request_size} exceeds the prescribed limit of {batch_limit}.")
+    else:
+        result = model.generate(request)
+        return result
 
 
 @service.route("/get_activations", methods=["POST"])
