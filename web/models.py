ad2957151ffc50fc5879fdc905820b62257613aa
John Willes <johnwilles@gmail.com>
2023-03-31 00:23:36 -0400

    Fix query

--- web/models.py
+++ web/models.py
@@ -314,7 +314,7 @@ class ModelInstance(BaseMixin, db.Model):
         return self._state.is_timed_out(timeout)
     
     def last_generation(self):
-        last_generation = db.select(ModelInstanceGeneration).where(model_instance_id=self.id).order_by(ModelInstanceGeneration.created_at.desc()).first()
+        last_generation = db.select(ModelInstanceGeneration).where(ModelInstanceGeneration.model_instance_id == self.id).order_by(ModelInstanceGeneration.created_at.desc()).first()
         current_app.logger.info(f"[last_generation] last_generation={last_generation}")
         if last_generation:
             current_app.logger.info(f"[last_generation] last_generation={last_generation.id}, {last_generation.created_at}")
