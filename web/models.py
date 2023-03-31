01622ac0575e0bcef9600d3f58bdea5387abc0ca
John Willes <johnwilles@gmail.com>
2023-03-31 00:14:55 -0400

    Add direct loading of last generation

--- web/models.py
+++ web/models.py
@@ -314,7 +314,11 @@ class ModelInstance(BaseMixin, db.Model):
         return self._state.is_timed_out(timeout)
     
     def last_generation(self):
-        return self.generations[-1] if self.generations else None
+        last_generation = db.select(ModelInstanceGeneration).where(model_instance_id=self.id).order_by(ModelInstanceGeneration.created_at.desc()).first()
+        current_app.logger.info(f"[last_generation] last_generation={last_generation}")
+        if last_generation:
+            current_app.logger.info(f"[last_generation] last_generation={last_generation.id}, {last_generation.created_at}")
+        return last_generation if last_generation else None
     
     def serialize(self):
         return {
