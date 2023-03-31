2f7b41fa35c5d2c6493003807238b8c4dd17d811
John Willes <johnwilles@gmail.com>
2023-03-30 23:08:07 -0400

    Add logging

--- web/tasks.py
+++ web/tasks.py
@@ -4,11 +4,15 @@ from celery import shared_task
 from models import ModelInstance
 from config import Config
 
+from flask import current_app
+
 
 @shared_task
 def verify_model_instance_health():
     current_model_instances = ModelInstance.find_current_instances()
     for model_instance in current_model_instances:
+        current_app.logger.info(model_instance.id)
+        current_app.logger.info(model_instance.state_name)
         if not model_instance.is_healthy() or model_instance.is_timed_out(Config.MODEL_INSTANCE_TIMEOUT):
             model_instance.shutdown()
 
