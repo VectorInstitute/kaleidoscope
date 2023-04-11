f39c7a050550dbc268f11e2daf4e30162ef8f67c
jsiva <jsiva@llm.cluster.local>
2023-04-11 14:36:10 -0400

    Add gunicorn config

---
+++ web/gunicorn.conf.py
@@ -0,0 +1,46 @@
+from config import Config
+import multiprocessing
+
+bind = f"{Config.GATEWAY_HOST}:{Config.GATEWAY_PORT}"
+workers = multiprocessing.cpu_count() * 2 + 1
+wsgi_app = "gateway_service:app"
+accesslog = "-"
+loglevel = "info"
+errorlog = "-"
+
+# Events for worker life cycle
+def post_fork(server, worker):
+    server.log.info("Worker spawned (pid: %s)", worker.pid)
+
+
+def pre_fork(server, worker):
+    pass
+
+
+def pre_exec(server):
+    server.log.info("Forked child, re-executing.")
+
+
+def when_ready(server):
+    server.log.info("Server is ready. Spawning workers")
+
+
+def worker_int(worker):
+    worker.log.info("worker received INT or QUIT signal")
+
+    ## get traceback info
+    import threading, sys, traceback
+
+    id2name = {th.ident: th.name for th in threading.enumerate()}
+    code = []
+    for threadId, stack in sys._current_frames().items():
+        code.append("\n# Thread: %s(%d)" % (id2name.get(threadId, ""), threadId))
+        for filename, lineno, name, line in traceback.extract_stack(stack):
+            code.append('File: "%s", line %d, in %s' % (filename, lineno, name))
+            if line:
+                code.append("  %s" % (line.strip()))
+    worker.log.debug("\n".join(code))
+
+
+def worker_abort(worker):
+    worker.log.info("worker received SIGABRT signal")
