484c340bcc3a1df0aab548a499051ec301a9a82f
jsiva <jsiva@llm.cluster.local>
2023-04-18 15:07:16 -0400

    Reorganize prod files

--- web/gateway_service.py
+++ web/gateway_service.py
@@ -62,6 +62,5 @@ def make_celery(app):
 app = create_app()
 celery = make_celery(app)
 
-
-# if __name__ == "__main__":
-#     app.run(host=Config.GATEWAY_HOST, port=Config.GATEWAY_PORT)
+if __name__ == "__main__":
+    app.run(host=Config.GATEWAY_BIND_HOST, port=Config.GATEWAY_PORT)
