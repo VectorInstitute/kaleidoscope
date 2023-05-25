675245927dad166db3da09759a5b6d2cb4fd19fc
Mark Coatsworth <mark.coatsworth@vectorinstitute.ai>
2023-05-25 09:19:12 -0400

    Fixed spacing in web/tasks.py (not sure how this got changed?)

--- web/tasks.py
+++ web/tasks.py
@@ -4,6 +4,7 @@ from celery import shared_task
 from models import ModelInstance
 from config import Config
 
+
 @shared_task
 def verify_model_instance_health():
     """Ensure model instances are health else shutdown"""
@@ -14,11 +15,13 @@ def verify_model_instance_health():
         ):
             model_instance.shutdown()
 
+
 @shared_task
 def launch_model_instance(model_instance_id):
     """Launch a model instance by id"""
     model_instance = ModelInstance.find_by_id(model_instance_id)
     model_instance.launch()
+
 
 @shared_task
 def shutdown_model_instance(model_instance_id):
