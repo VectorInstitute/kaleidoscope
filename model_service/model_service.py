ae995e09e8e89c9c98a5187f27b3af69d1a21ab2
John Willes <johnwilles@gmail.com>
2023-05-30 19:15:56 -0400

    Add activation/registration back into model flow

--- model_service/model_service.py
+++ model_service/model_service.py
@@ -6,43 +6,57 @@ from pytriton.triton import Triton, TritonConfig
 
 from services.gateway_service import GatewayServiceClient
 
-
-
 logger = logging.getLogger("kaleidoscope.model_service")
 logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(name)s: %(message)s")
 
-
 def initialize_model(model_type):
-    #try:
+    """Initializes model based on model type
+    Args:
+        model_type (str): Type of model to load
+    """
     return importlib.import_module(f"models.{model_type}.model").Model(model_type)
-    # except:
-    #     logger.error(f"Could not import model {model_type}")
 
 class ModelService():
+    ''' Model service is responsible for loading and serving a model.
+    '''
 
-    def __init__(self, model_type, model_path, gateway_host, gateway_port, master_port) -> None:
+    def __init__(self, model_instance_id: str, model_type: str, model_path: str, gateway_host: str, gateway_port: int, master_host: str, master_port: int) -> None:
+        """
+        Args:
+            model_instance_id (str): Unique identifier for model instance
+            model_type (str): Type of model to load
+            model_path (str): Path to pre-trained model
+            gateway_host (str): Hostname for gateway service
+            gateway_port (int): Port for gateway service
+            master_host (str): Hostname for master service
+            master_port (int): Port for master service
+        """
+        self.model_instance_id = model_instance_id
         self.model_type = model_type
-        self.gateway_host = gateway_host
-        self.gateway_port = gateway_port
-        self.master_port = master_port
         self.model_path = model_path
 
-    def run(self):
+        self.gateway_host = gateway_host
+        self.gateway_port = gateway_port
+    
+        self.master_host = master_host
+        self.master_port = master_port
 
-        # Register model with gateway -> note how to run this only on rank 0
-        logger.info(f"Registering model {self.model_type} with gateway")
+    def run(self):
+        """Loads model and starts serving requests
+        """
+
         gateway_service = GatewayServiceClient(self.gateway_host, self.gateway_port)
         gateway_service.register_model_instance(self.model_type, self.master_host, self.master_port)
-        #gateway_service.register_model(self.model_type, self.master_host, self.master_port)
 
         model = initialize_model(self.model_type)
         model.load(self.model_path)
 
         if model.rank == 0:
             logger.info(f"Starting model service for {self.model_type} on rank {model.rank}")
+            gateway_service.activate_model_instance(self.model_instance_id)
 
             #Placeholder static triton config for now
-            triton_config = TritonConfig(http_address="0.0.0.0", http_port=8003, log_verbose=4)
+            triton_config = TritonConfig(http_address="0.0.0.0", http_port=self.master_port, log_verbose=4)
             with Triton(config=triton_config) as triton:
                 triton = model.bind(triton)
                 triton.serve()
@@ -51,26 +65,30 @@ class ModelService():
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--model_type",
-        required=True,
-        type=str,
+        "--model_type", required=True, type=str,
     )
     parser.add_argument(
         "--model_path", required=True, type=str, help="Path to pre-trained model"
     )
-    parser.add_argument("--model_instance_id", required=True, type=str)
     parser.add_argument(
-        "--gateway_host", required=False, type=str, help="Hostname of gateway service", default="llm.cluster.local"
+        "--model_instance_id", required=True, type=str
     )
     parser.add_argument(
-        "--gateway_port", required=False, type=int, help="Port of gateway service", default=3001
+        "--master_host", required=True, type=str, help="Hostname for device communication"
     )
     parser.add_argument(
-        "--master_port", required=False, type=int, help="Port for device communication", default=29400
+        "--master_port", required=True, type=int, help="Port for device communication"
+    )
+    parser.add_argument(
+        "--gateway_host", required=False, type=str, help="Hostname of gateway service", default=None
+    )
+    parser.add_argument(
+        "--gateway_port", required=False, type=int, help="Port of gateway service", default=None
     )
     args = parser.parse_args()
 
-    model_service = ModelService(args.model_type, args.model_path, args.gateway_host, args.gateway_port, args.master_port)
+    # ToDo: Better init of model service
+    model_service = ModelService(**args)
     model_service.run()
 
 
--- model_service/services/gateway_service.py
+++ model_service/services/gateway_service.py
@@ -1,4 +1,7 @@
+import logging
 import requests
+
+logger = logging.getLogger(__name__)
 
 class GatewayServiceClient():
 
@@ -6,13 +9,43 @@ class GatewayServiceClient():
         self.host = host
         self.port = port
 
-    def register_model_instance(model_type, master_host, master_port):
-        # url = f"http://{self.host}:{self.port}/model_instance"
-        # data = {
-        #     "model_type": model_type,
-        #     "master_host": master_host,
-        #     "master_port": master_port
-        # }
-        # response = requests.post(url, json=data)
-        # return response
-        pass
+    def register_model_instance(self, model_instance_id, model_host, model_port):
+        """Registers model instance with gateway service
+
+        Args:
+            model_instance_id (str): Unique identifier for model instance
+            model_host (str): Hostname for model service
+            model_port (int): Port for model service
+        """
+        model_instance_registration_url =  f"http://{self.host}/models/instances/{model_instance_id}/register"
+
+        registration_data = {
+            "host": model_host,
+            "port": model_port
+        }
+
+        logger.info(
+            f"Sending model registration request to {model_instance_registration_url} with data: {registration_data}"
+        )
+
+        response = requests.post(model_instance_registration_url, json=registration_data)
+        # HTTP error codes between 450 and 500 are custom to the kaleidoscope gateway
+        if int(response.status_code) >= 450 and int(response.status_code) < 500:
+            raise requests.HTTPError(response.content.decode("utf-8"))
+        
+    def activate_model_instance(self, model_instance_id):
+        """Activates model instance with gateway service
+
+        Args:
+            model_instance_id (str): Unique identifier for model instance
+        """
+
+        model_instance_activation_url = f"http://{self.host}/models/instances/{model_instance_id}/activate"
+
+        logger.info(
+            f"Sending model activation request to {model_instance_activation_url}"
+        )
+        response = requests.post(model_instance_activation_url)
+        # HTTP error codes between 450 and 500 are custom to the kaleidoscope gateway
+        if int(response.status_code) >= 450 and int(response.status_code) < 500:
+            raise requests.HTTPError(response.content.decode("utf-8"))
