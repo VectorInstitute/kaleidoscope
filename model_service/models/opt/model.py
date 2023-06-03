f692430eccb580ad706758934160f92bd85b2158
John Willes <johnwilles@gmail.com>
2023-06-03 09:41:52 -0400

    Remove inputs with python types

--- model_service/models/opt/model.py
+++ model_service/models/opt/model.py
@@ -89,14 +89,14 @@ class Model(AbstractModel):
         
     def bind(self, triton):
         triton.bind(
-            model_name=f"{self.model_name}_inference",
+            model_name=f"{self.model_name}_generation",
             infer_func=self.infer,
             inputs=[
                 Tensor(name="prompts", dtype=bytes, shape=(1,)),
-                Tensor(name='max_tokens', dtype=int, shape=(1,), optional=True),
-                Tensor(name='min_tokens', dtype=int, shape=(1,), optional=True),
-                Tensor(name='temperature', dtype=float, shape=(1,), optional=True),
-                Tensor(name='top_p', dtype=int, shape=(1,), optional=True),
+                # Tensor(name='max_tokens', dtype=np.int, shape=(1,), optional=True),
+                # Tensor(name='min_tokens', dtype=int, shape=(1,), optional=True),
+                # Tensor(name='temperature', dtype=float, shape=(1,), optional=True),
+                # Tensor(name='top_p', dtype=int, shape=(1,), optional=True),
             ],
             outputs=[
                 Tensor(name="sequences", dtype=bytes, shape=(-1,)),
@@ -106,25 +106,25 @@ class Model(AbstractModel):
             ],
             config=ModelConfig(max_batch_size=128),
         )
-        triton.bind(
-            model_name=f"{self.model_name}_activations",
-            infer_func=self.get_activations,
-            inputs=[
-                Tensor(name="prompts", dtype=bytes, shape=(1,)),
-                Tensor(name='max_tokens', dtype=int, shape=(1,), optional=True),
-                Tensor(name='min_tokens', dtype=int, shape=(1,), optional=True),
-                Tensor(name='temperature', dtype=float, shape=(1,), optional=True),
-                Tensor(name='top_p', dtype=int, shape=(1,), optional=True),
-                Tensor(name='encoded_activation_payload', dtype=bytes, shape=(1,)),
-            ],
-            outputs=[
-                Tensor(name="sequences", dtype=bytes, shape=(-1,)),
-                # Tensor(name="text", dtype=bytes, shape=(-1,)),
-                # Tensor(name="tokens", dtype=bytes, shape=(-1,)),
-                # Tensor(name="logprobs", dtype=bytes, shape=(-1,)),
-            ],
-            config=ModelConfig(max_batch_size=128),
-        )
+        # triton.bind(
+        #     model_name=f"{self.model_name}_activations",
+        #     infer_func=self.get_activations,
+        #     inputs=[
+        #         Tensor(name="prompts", dtype=bytes, shape=(1,)),
+        #         Tensor(name='max_tokens', dtype=int, shape=(1,), optional=True),
+        #         Tensor(name='min_tokens', dtype=int, shape=(1,), optional=True),
+        #         Tensor(name='temperature', dtype=float, shape=(1,), optional=True),
+        #         Tensor(name='top_p', dtype=int, shape=(1,), optional=True),
+        #         Tensor(name='encoded_activation_payload', dtype=bytes, shape=(1,)),
+        #     ],
+        #     outputs=[
+        #         Tensor(name="sequences", dtype=bytes, shape=(-1,)),
+        #         # Tensor(name="text", dtype=bytes, shape=(-1,)),
+        #         # Tensor(name="tokens", dtype=bytes, shape=(-1,)),
+        #         # Tensor(name="logprobs", dtype=bytes, shape=(-1,)),
+        #     ],
+        #     config=ModelConfig(max_batch_size=128),
+        # )
         return triton 
 
     @property
