1a1f5f4e0af32ebaa1f479071220bb2ae9e69d71
Mark Coatsworth <mark.coatsworth@vectorinstitute.ai>
2023-06-27 15:00:41 -0400

    Re-enabled opt generation attributes, changed port

--- model_service/models/opt/model.py
+++ model_service/models/opt/model.py
@@ -116,8 +116,8 @@ class Model(AbstractModel):
                 Tensor(name='min_tokens', dtype=np.int64, shape=(1,), optional=True),
                 Tensor(name='temperature', dtype=np.float64, shape=(1,), optional=True),
                 Tensor(name='top_p', dtype=np.int64, shape=(1,), optional=True),
-                # Tensor(name='top_k', dtype=np.int16, shape=(1,), optional=True),
-                # Tensor(name='repetition_penalty', dtype=np.float32, shape=(1,), optional=True),
+                Tensor(name='top_k', dtype=np.int16, shape=(1,), optional=True),
+                Tensor(name='repetition_penalty', dtype=np.float32, shape=(1,), optional=True),
                 Tensor(name='encoded_activation_payload', dtype=bytes, shape=(1,), optional=True),
                 Tensor(name='echo', dtype=np.bool_, shape=(1,), optional=True)
             ],
@@ -138,8 +138,8 @@ class Model(AbstractModel):
                 Tensor(name='min_tokens', dtype=np.int64, shape=(1,), optional=True),
                 Tensor(name='temperature', dtype=np.float64, shape=(1,), optional=True),
                 Tensor(name='top_p', dtype=np.int64, shape=(1,), optional=True),
-                # Tensor(name='top_k', dtype=np.int16, shape=(1,), optional=True),
-                # Tensor(name='repetition_penalty', dtype=np.float32, shape=(1,), optional=True),
+                Tensor(name='top_k', dtype=np.int16, shape=(1,), optional=True),
+                Tensor(name='repetition_penalty', dtype=np.float32, shape=(1,), optional=True),
                 Tensor(name='encoded_activation_payload', dtype=bytes, shape=(1,), optional=True),
                 Tensor(name='echo', dtype=np.bool_, shape=(1,), optional=True)
             ],
