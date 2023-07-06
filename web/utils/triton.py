Fixed logprobs decoding for >1 sequences

web/utils/triton.py
@@ -73,12 +73,16 @@ class TritonClient():
         inputs_wrapped = prepare_inputs(inputs, task_config['input'])
 
         response = self._client.infer(model_bind_name, inputs_wrapped)
-        
         sequences = np.char.decode(response.as_numpy("sequences").astype("bytes"), "utf-8").tolist()
         tokens = np.char.decode(response.as_numpy("tokens").astype("bytes"), "utf-8").tolist()
-        
         logprobs = np.char.decode(response.as_numpy("logprobs").astype("bytes"), "utf-8").tolist()
+
+        # Logprobs need special treatment because they are encoded as bytes
+        # Regular np float arrays don't work, each element has a different number of items
         for i in range(len(logprobs)):
+            # They are also formatted differently for >1 sequences
+            if len(logprobs) > 1:
+                logprobs[i] = logprobs[i][1:-1].split(', ')
             logprobs[i] = [float(prob) if prob!="None" else None for prob in logprobs[i]]
 
         result = {