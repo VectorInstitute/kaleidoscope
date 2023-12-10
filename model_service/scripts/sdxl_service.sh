81e1c8b9a60cf430b6f00af24bf80cbe7de16a1e
Mark Coatsworth <mark.coatsworth@vectorinstitute.ai>
2023-12-10 15:07:03 -0500

    Demo scripts for SDXL generation

--- model_service/scripts/sdxl_service.sh
+++ model_service/scripts/sdxl_service.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=sdxl_triton_service
-#SBATCH --mail-type=END,FAIL
-#SBATCH --ntasks=1
+#SBATCH --job-name=sdxl_service
+#SBATCH --nodes=1
+#SBATCH --mem=16G
+#SBATCH --ntasks-per-node=1
 #SBATCH --gres=gpu:1
-#SBATCH --output=sdxl_triton_service.%j.out
-#SBATCH --error=sdxl_triton_service.%j.err
+#SBATCH --cpus-per-task=4
+#SBATCH --time=3-00:00
+#SBATCH --output=sdxl_service.%j.out
+#SBATCH --error=sdxl_service.%j.err
 
 source /opt/lmod/lmod/init/profile
 module load singularity-ce/3.8.2
-
-singularity exec --nv --bind /scratch/ssd004,/ssd005,/scratch,/checkpoint /ssd005/projects/llm/triton/pytriton_base.sif /usr/bin/python3 -s ../model_service.py --model_type sdxl --model_variant None --model_path /model-weights/sdxl-turbo --model_instance_id test --gateway_host 127.0.0.1 --gateway_port 0 --master_host localhost --master_port 8003
+singularity exec --nv --bind /scratch/ssd004,/ssd005,/scratch,/model-weights /ssd005/projects/llm/sdxl-latest.sif /usr/bin/python3 -s ../model_service.py --model_type sdxl --model_variant None --model_path /model-weights/sdxl-turbo --model_instance_id test --gateway_host 127.0.0.1 --gateway_port 0 --master_host localhost --master_port 8003
